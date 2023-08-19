f0dc21855731d004d54c34f738384198711edd8e
S B Chand Basha <124661910+chandbasha304@users.noreply.github.com>
2023-08-19 11:33:43 +0530

    Update ClientRequest.java

--- src/main/java/com/controller/ClientRequest.java
+++ src/main/java/com/controller/ClientRequest.java
@@ -3,7 +3,7 @@ package com.controller;
 import java.util.List;
 
 import java.util.Optional;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +21,7 @@ import com.service.ParkingOperations;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-@RestController
+@Controller
 public class ClientRequest {
 
 
